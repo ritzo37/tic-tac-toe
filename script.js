e5e7b73b96a7fe932e826cbe1aaf92d0c70d8d88
ritzo37 <ritishrai56@gmail.com>
2024-06-18 05:06:04 +0530

    Update console-based game to GUI-based game

--- script.js
+++ script.js
@@ -41,6 +41,10 @@ function winnerCheck(board) {
         boardValues.push(currRowValues);
     }
 
+    for (let i = 0 ; i<3 ; i++) {
+        console.log(boardValues[i]) ;
+    }
+
     function rowCheck() {
         for (let i = 0; i < 3; i++) {
             let xCnt = 0, oCnt = 0;
@@ -51,7 +55,10 @@ function winnerCheck(board) {
                     oCnt++;
                 }
             }) 
-            if (xCnt == 3 || oCnt == 3) return true;  
+            if (xCnt == 3 || oCnt == 3) {
+                console.log("Row Check True") ;
+                return true ;
+            }
            
         }
         return false ;
@@ -66,11 +73,15 @@ function winnerCheck(board) {
                     oCnt ++ ;
                 }
             }
-            if (xCnt ==3 || oCnt ==3 ) return true ;
+            if (xCnt ==3 || oCnt ==3 ) {
+                console.log("Coln Check true") ; 
+                return true ;
+            }
         }
         return false ;
     }
     function diagonalCheck() {
+
         let i = 0 , j = 0 ; 
         let xCnt = 0 , oCnt = 0 ;
         while (i<3 && j<3) {
@@ -80,8 +91,24 @@ function winnerCheck(board) {
             }
             i++ ; j++ ;
         }
-        if (xCnt == 3 || oCnt == 3) return true ;
-        return false; 
+
+        if (xCnt == 3 || oCnt == 3) {
+            return true ;
+        }
+
+        
+        i = 0, j = 2 , xCnt = 0 , oCnt = 0 ;
+        while (i >=0 && j>=0) {
+             if (boardValues[i][j] == 'X') xCnt++ ; 
+             else if (boardValues[i][j] == 'O') oCnt++ ;
+             i++ ; j-- ;
+        }
+
+        if (xCnt == 3 || oCnt == 3) {
+            return true ;
+        }
+
+        return false ;
     }
 
     return (rowCheck() || diagonalCheck() || colnCheck());
@@ -94,6 +121,7 @@ function drawCheck(board) {
             if (cell.getCharacter() != '') cnt++ ;
         })
     })
+    console.log(cnt) ;
     return (cnt == 9) ;
 }
 
@@ -120,8 +148,8 @@ function gameControler() {
         let currCharacter = activePlayer.char;
         let board = boardObj.getBoard();
         board[row][column].changeCharacter(currCharacter);
-        let gameEndFlag = winnerCheck(boardObj.getBoard());
-        let drawFlag = drawCheck(boardObj.getBoard()); 
+        let gameEndFlag = winnerCheck(board);
+        let drawFlag = drawCheck(board); 
         if (gameEndFlag) {
             return "WIN"; 
         } 
@@ -129,60 +157,84 @@ function gameControler() {
             return "DRAW" ;
         }
         changeActivePlayer();
-        return false ;
+        return "CONTINUE" ;
     }
-
-    function printBoard() {
-        let currBoard = boardObj.getBoard();
-
-        for (let i = 0; i < 3; i++) {
-            let currRow = currBoard[i].map((Cell) => {
-                return Cell.getCharacter();
-            });
-            console.log(currRow);
-        }
-    }
+    
+    const getBoard = ()=> boardObj.getBoard() ;
 
     return {
         playMatch,
-        Board: boardObj.getBoard(),
-        printBoard,
+        getBoard , 
         getActivePlayer ,
     }
 }
+
+
+function ScreenController() {
+
+     let gameControl = gameControler() ;  
+     let board = gameControl.getBoard() ;
+     let playMatch = gameControl.playMatch ;  
+     let gameEndFlag = false ;
+     
+     const container = document.querySelector('.container');
+     const resultConatiner = document.querySelector('.resultDisplay') ;
+      
+    const updateScreen = ()=> {
+        
+        container.textContent = ""; 
+        for (let i =0 ; i<3 ; i++) { 
+            const row = document.createElement('div') ;
+            row.classList.add('row') ;
+            for (let j =0  ; j<3 ; j++) {
+                const div = document.createElement('div') ;
+                div.classList.add('cell') ;
+                div.dataset.row = i ; 
+                div.dataset.column = j ;
+                div.textContent = board[i][j].getCharacter(); 
+                row.appendChild(div) ;
+            }
+            container.appendChild(row) ;
+        }
+
+    }
+
+    container.addEventListener('click', (event)=> {
+        let row = event.target.getAttribute("data-row") ;
+        let coln = event.target.getAttribute("data-column") ;
+        
+        console.log(row + coln) ;
+        if (row && coln && board[row][coln].getCharacter() == '' && !gameEndFlag) {
+            let result = playMatch(row,coln) ;
+
+            if (result == "WIN") {
+                updateScreen() ;
+                gameEndFlag = true ;
+                const activePlayer = gameControl.getActivePlayer() ; 
+                const activePlayerName = activePlayer.name ;
+                resultConatiner.textContent = `${activePlayerName} is the winner !` ;
+            }
+            else if (result == "DRAW") {
+                gameEndFlag = true ;
+                updateScreen() ;
+                resultConatiner.textContent = "The game ended in a draw !" ;
+            }
+            else {
+                updateScreen() ;
+            }
+        }
+  
+    })
+
+    return { 
+        updateScreen , 
+    }
+
+}
+
+const Screen = ScreenController() ; 
+Screen.updateScreen() ;
  
 
 
-const gameC = gameControler();
-while (true) {
-     let activePlayer = gameC.getActivePlayer() ;
-     let activePlayerName = activePlayer.name ; 
-     let activePlayerCharacter = activePlayer.char ;
 
-     console.log(activePlayerName + " " + " turn " + "...") ; 
-     console.log("Please row and coln for the character " + activePlayerCharacter) ;
-     let row = prompt("Enter row to place your char") ; 
-     let coln = prompt("Enter coln to place your char") ;
-
-     if (!(row>=0 && row<=2) || !(coln>=0 && coln<=2)) {
-         console.log("Please Enter valid row's and columns ! ") ; 
-         continue ;
-     }
-
-     let currBoard = gameC.Board ;
-     if (currBoard[row][coln].getCharacter() != '') {
-        console.log("Please Enter the row and coln where the character isn't filled already !") ; 
-        continue ;
-     }
-
-     let currGameResult = gameC.playMatch(row,coln) ;
-     gameC.printBoard() ;
-     if (currGameResult == "WIN") {
-        alert(activePlayerName + "is the Winner !") ;
-        break ;
-     }
-     else if (currGameResult == "DRAW") {
-        alert ("The game ended in a god damn draw!") ; 
-        break ;
-     }
-}
